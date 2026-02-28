bd6d7d1392f04be3c379b074d17814e2a7c9214d
Murli Kumar <aimgodhere@gmail.com>
2026-03-01 00:06:46 +0530

    commit after router implementation

--- Express/Router/src/app.js
+++ Express/Router/src/app.js
@@ -1,23 +1,12 @@
 const express = require("express");
+const orderRouter = require("./routes/orders.js");
+const userRouter = require("./routes/users.js");
 
 const app = express();
 const port = 3000;
 
-app.get("/orders", (req, res) => {
-  res.send("Order List!");
-});
-
-app.post("/orders", (req, res) => {
-  res.send("Order Created!");
-});
-
-app.get("/users", (req, res) => {
-  res.send("User List");
-});
-
-app.post("/users", (req, res) => {
-  res.send("User Created!");
-});
+app.use("/orders", orderRouter);
+app.use("/users", userRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
