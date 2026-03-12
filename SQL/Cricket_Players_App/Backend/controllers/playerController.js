a4001e4fbaade8e5c3674297192664f77df2b614
Murli Kumar <aimgodhere@gmail.com>
2026-03-12 13:10:07 +0530

    destructure the userDetails in logic code

--- SQL/Cricket_Players_App/Backend/controllers/playerController.js
+++ SQL/Cricket_Players_App/Backend/controllers/playerController.js
@@ -1,37 +1,111 @@
 const Player = require("../models/playersTable");
 
 const addPlayer = async (req, res) => {
-  const player = await Player.create(req.body);
+  try {
+    const {
+      name,
+      dob,
+      photoUrl,
+      birthplace,
+      career,
+      matches,
+      score,
+      fifties,
+      centuries,
+      wickets,
+      average,
+    } = req.body;
 
-  res.json(player);
+    const player = await Player.create({
+      name,
+      dob,
+      photoUrl,
+      birthplace,
+      career,
+      matches,
+      score,
+      fifties,
+      centuries,
+      wickets,
+      average,
+    });
+
+    res.json(player);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 const searchPlayer = async (req, res) => {
-  const player = await Player.findOne({
-    where: { name: req.params.name },
-  });
+  try {
+    const { name } = req.params;
 
-  res.json(player);
+    const player = await Player.findOne({
+      where: { name },
+    });
+
+    res.json(player);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
-
 const updatePlayer = async (req, res) => {
-  const id = req.params.id;
+  try {
+    const { id } = req.params;
 
-  await Player.update(req.body, {
-    where: { id },
-  });
+    const {
+      name,
+      dob,
+      photoUrl,
+      birthplace,
+      career,
+      matches,
+      score,
+      fifties,
+      centuries,
+      wickets,
+      average,
+    } = req.body;
 
-  const updatedPlayer = await Player.findByPk(id);
+    await Player.update(
+      {
+        name,
+        dob,
+        photoUrl,
+        birthplace,
+        career,
+        matches,
+        score,
+        fifties,
+        centuries,
+        wickets,
+        average,
+      },
+      {
+        where: { id },
+      },
+    );
 
-  res.send(updatedPlayer);
+    const updatedPlayer = await Player.findByPk(id);
+
+    res.send(updatedPlayer);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 const deletePlayer = async (req, res) => {
-  await Player.destroy({
-    where: { id: req.params.id },
-  });
+  try {
+    const { id } = req.params;
 
-  res.send("Player Deleted");
+    await Player.destroy({
+      where: { id },
+    });
+    
+    res.send("Player Deleted");
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 module.exports = { addPlayer, searchPlayer, updatePlayer, deletePlayer };
